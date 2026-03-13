e082e588e9a59e306664fca653b8cf83f7d7f553
Alex Kang <alexbkang@gmail.com>
2026-03-13 14:39:52 -0600

    update

--- lua/undodiff/init.lua
+++ lua/undodiff/init.lua
@@ -1,7 +1,12 @@
 local M = {}
 local state = require("undodiff.state")
 
-function M.setup(_opts)
+function M.setup(opts)
+	M.opts = vim.tbl_deep_extend("force", {
+		treesitter = true,
+		number = true,
+		signcolumn = "no",
+	}, opts or {})
 	vim.api.nvim_create_user_command("UndoDiff", function()
 		M.attach()
 	end, { desc = "Open undodiff" })
@@ -24,6 +29,7 @@ function M.attach()
 	vim.bo[tree_buf].bufhidden = "wipe"
 	require("undotree").open({ bufnr = tree_buf })
 	local tree_win = vim.api.nvim_get_current_win()
+	vim.wo[tree_win].winhighlight = "Normal:UndoDiffTree"
 
 	vim.api.nvim_set_current_win(old_win)
 	vim.cmd("vsplit")
@@ -38,12 +44,41 @@ function M.attach()
 	state.tree_win = tree_win
 	state.old_win = old_win
 	state.curr_buf = curr_buf
+	state.confirmed = false
+	state.original_seq = vim.fn.undotree(curr_buf).seq_cur
 	state.set_active(state.buffers)
 
+	vim.b[new_buf].lsp_disable = true
+	vim.b[old_buf].lsp_disable = true
 	vim.bo[new_buf].filetype = ft
 	vim.bo[old_buf].filetype = ft
+	if not M.opts.treesitter then
+		vim.treesitter.stop(new_buf)
+		vim.treesitter.stop(old_buf)
+	end
 	vim.wo[old_win].scrollbind = true
 	vim.wo[new_win].scrollbind = true
+	for _, win in ipairs({ old_win, new_win }) do
+		vim.wo[win].number = M.opts.number
+		vim.wo[win].signcolumn = M.opts.signcolumn
+		vim.wo[win].winhighlight = "Normal:UndoDiffView"
+	end
+
+	local function sync_cursor(from_win, to_win)
+		local cursor = vim.api.nvim_win_get_cursor(from_win)
+		if vim.api.nvim_win_is_valid(to_win) then
+			vim.api.nvim_win_set_cursor(to_win, cursor)
+		end
+	end
+
+	vim.api.nvim_create_autocmd("CursorMoved", {
+		buffer = old_buf,
+		callback = function() sync_cursor(old_win, new_win) end,
+	})
+	vim.api.nvim_create_autocmd("CursorMoved", {
+		buffer = new_buf,
+		callback = function() sync_cursor(new_win, old_win) end,
+	})
 
 	require("codediff").setup({})
 	require("undodiff.render").setup(tree_buf, curr_buf, old_buf, new_buf, old_lines)
@@ -57,6 +92,11 @@ function M.attach()
 			state.clear(state.buffers, state.scratch_win)
 		end,
 	})
+
+	vim.keymap.set("n", "<CR>", function()
+		state.confirmed = true
+		vim.api.nvim_win_close(tree_win, true)
+	end, { buffer = tree_buf, desc = "UndoDiff: confirm and close" })
 
 	vim.api.nvim_set_current_win(tree_win)
 end
--- lua/undodiff/state.lua
+++ lua/undodiff/state.lua
@@ -11,6 +11,13 @@ function M.set_active(buffers)
 end
 
 function M.clear(buffers, scratch_win)
+	if not M.confirmed and M.curr_buf and M.original_seq then
+		vim.api.nvim_buf_call(M.curr_buf, function()
+			vim.cmd("silent undo " .. M.original_seq)
+		end)
+	end
+	M.confirmed = nil
+	M.original_seq = nil
 	if M.old_win and vim.api.nvim_win_is_valid(M.old_win) and M.curr_buf then
 		vim.api.nvim_win_set_buf(M.old_win, M.curr_buf)
 		M.old_win = nil
